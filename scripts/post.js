cf8d0b12f33b7b5d0480d6cf22893c1f127e20f6
Pritam Sen <pritamsen@gmail.com>
2020-08-24 21:35:43 +0200

    bugfixes
    bugfixes

--- scripts/post.js
+++ scripts/post.js
@@ -1,37 +1,18 @@
-
-//like function
-var numberOfClicks=0;
-function postLiked(){
-   var button = document.getElementById("addLikes")
-   button.innerHTML = '<i class="fa fa-thumbs-up ">'+'</i>'+'&nbsp;'+"Liked!";
-   button.style.border="none";
-    numberOfClicks += 1;
-    if(numberOfClicks!=0){
-        if (numberOfClicks==1){
-            document.getElementById('likeCount').innerHTML = numberOfClicks + " person likes this!";
-        }
-        else {
-            document.getElementById('likeCount').innerHTML = numberOfClicks + " people have liked this!";
-        }
-        }
-    }
-
-//comments function..
 function addComment(id){
    if (id.value.length>0){
     var addEventName = id.value ;
-    document.getElementById('addEventNames').innerHTML += "UPDATED"+'<p id="comment">' + addEventName + '</p>' ; 
+    document.getElementById('addEventNames').innerHTML += '<p id="comment">' + addEventName + '</p>' ; 
     var a = document.getElementById('userComments');         
     a.value=a.defaultValue;
    }
 }
 
 var titleContent = document.getElementById('postTitleNew');
+var updatedtitleContent= 'UPDATED: ' + titleContent.innerHTML;
 var contentText = document.getElementById('postBody');
 var editButton = document.getElementById('editButton');
 var saveButton = document.getElementById('saveButton');
 
-// edit and save the content
 function editContent(){
     contentText.contentEditable="true";
     titleContent.contentEditable="true";
@@ -39,7 +20,6 @@ function editContent(){
     titleContent.style.border="1px solid red";
     editButton.style.display="none";
     saveButton.style.display="block";
-    contentText.focus();
 }
 
 function saveContent(){
@@ -50,3 +30,19 @@ function saveContent(){
     editButton.style.display="block";
     saveButton.style.display="none"; 
 }
+
+var numberOfClicks=0;
+function AddLikes(){
+   var button = document.getElementById("addLikes")
+   button.innerHTML = '<i class="fa fa-thumbs-up ">'+'</i>'+'&nbsp;'+"Liked!";
+   button.style.border="none";
+    numberOfClicks += 1;
+    if(numberOfClicks!=0){
+        if (numberOfClicks==1){
+            document.getElementById('likeCount').innerHTML = numberOfClicks + " person likes this!";
+        }
+        else {
+            document.getElementById('likeCount').innerHTML = numberOfClicks + " people liked this!";
+        }
+        }
+    }
