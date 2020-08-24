b7cb0cce3880bd8f5ce2115c16428f4a65fafc18
Pritam Sen <pritamsen@gmail.com>
2020-08-24 22:01:37 +0200

    implemented elipses
    implemented elipses

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -2,3 +2,7 @@ var deleteModal = document.getElementById('deleteModal');
 function showDeleteModal() {
     deleteModal.style.display="flex";
 }
+
+function openPost() {
+	window.location.href = "./post.html";
+}
