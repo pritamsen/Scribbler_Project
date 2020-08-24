bfcbea355e1459c03c56493d37c0316e23d3dfeb
Pritam Sen <pritamsen@gmail.com>
2020-08-24 19:08:53 +0200

    update CSS & JS
    update CSS & JS

---
+++ scripts/common.js
@@ -0,0 +1,21 @@
+
+var showSignUp = function() {
+    var signup_modal = document.getElementById('signUppopUp');
+    signup_modal.style.display = "flex";
+}
+
+
+var hideSignUp = function(){
+    var signup_modal = document.getElementById('signUppopUp');
+    signup_modal.style.display = "none";
+}
+
+function showSignIn(){
+    var modal = document.getElementById("signInpopUp");
+    modal.style.display = "flex";
+}
+
+  function hideSignIn(){
+    var modal = document.getElementById("signInpopUp");
+    modal.style.display = "none";
+}
---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+
+function loadAllPosts(){
+    window.location.href = "./html/postslist.html";
+}
+function createPostpopUp() {
+    document.getElementById('postpopUp').style.display="flex";
+}
+
+function hidePostpopUp(){
+    document.getElementById('postpopUp').style.display="none";
+}
